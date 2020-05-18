85a31639e62260083c381d407469180397fed040
Magdalena Lipka <magalenalipka@int.pl>
2020-05-18 19:35:52 +0200

    08

---
+++ 08_Selection/Source.java
@@ -0,0 +1,33 @@
+//Magdalena Lipka gr. 1
+import java.util.Scanner;
+
+class Tabelka {
+	int[] tab;
+	int n;
+
+	Tabelka( int ilosc ) {
+		tab = new int[ilosc];
+		n = ilosc;
+	}
+
+	int find (int poz) {
+		int i = 0;
+		return i;
+	}
+}
+
+class Source {
+	public static Scanner scan = new Scanner (System.in);
+	public static void main ( String[] args ) {
+
+		int Quantity = scan.nextInt();
+
+		for ( int i = 0; i < Quantity; i++ ) {
+
+			
+
+		}
+
+	}
+
+}
