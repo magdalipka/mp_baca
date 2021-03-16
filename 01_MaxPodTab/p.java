b14707951424e416c339130f75c21fae542491af
Magda Lipka <dev@magdalipka.com>
2021-03-16 15:28:21 +0100

    comments

--- 01_MaxPodTab/p.java
+++ 01_MaxPodTab/p.java
@@ -1,3 +1,4 @@
+
 //Magdalena Lipka gr.7//
 import java.util.Scanner;
 
@@ -5,10 +6,13 @@ class Source {
 
     static Scanner scan = new Scanner(System.in);
 
+    // Liczenie pola prostokata - ilosc komoorek z podtabeli
     static int area(int top, int bottom, int left, int right) {
         return (bottom - top + 1) * (right - left + 1);
     }
 
+    // Sprawzdenie czy podtabela 1 jest lepsza (mniejsza rozmiarem, albo
+    // leksykograficznie) od podtabeli 2.
     static boolean isWinning(int top1, int bottom1, int left1, int right1, int top2, int bottom2, int left2,
             int right2) {
         if (area(top1, bottom1, left1, right1) < area(top2, bottom2, left2, right2)) {
@@ -44,12 +48,12 @@ class Source {
 
     public static void main(String[] args) {
 
-        int Quantity = scan.nextInt(); // ilosc zestawow//
+        int Quantity = scan.nextInt(); // ilosc zestawow
 
         while (Quantity > 0) {
 
             int SetNumber; // numer zestawu
-            int Columns, Lines; // wymiary pojedynczej tabeli//
+            int Columns, Lines; // wymiary pojedynczej tabeli
             SetNumber = scan.nextInt();
             scan.next();
             Lines = scan.nextInt();
@@ -64,8 +68,7 @@ class Source {
 
             boolean only_neg = true;
 
-            // wczytywanie//
-
+            // Wczytywanie
             for (int i = 0; i < Lines; i++) {
                 for (int j = 0; j < Columns; j++) {
                     int value = scan.nextInt();
@@ -75,22 +78,26 @@ class Source {
 
                 }
             }
+            // Poczatkowo uznajemy za 'wygrywajaca` podtablice pojedyncza pierwsza komorke.
             max_sum = Array[0][0];
 
             if (only_neg) {
                 System.out.println(SetNumber + ": n=" + Lines + " m=" + Columns + ", ms= 0, mstab is empty");
             } else {
 
+                // Dla podtabeli zaczynajacej sie na kazdej wysokosci.
                 for (int temp_top = 0; temp_top < Lines; temp_top++) {
 
+                    // Dla podtabeli o kazdej wysokosci.
                     for (int temp_height = 0; temp_height + temp_top < Lines; temp_height++) {
 
-                        // sumowanie podtabeli zacznajacej sie w temp_left o szerokowi temp_width//
-
+                        // Inicjalizacja listy sum kolumn oryginalnej podtabeli.
                         int[] Sum = new int[Columns];
                         for (int i = 0; i < Columns; i++)
                             Sum[i] = 0;
 
+                        // sumowanie podtabeli zacznajacej sie w temp_left o szerokosci temp_width do
+                        // pojedynczej listy.
                         for (int i = 0; i < Columns; i++) {
                             for (int j = temp_top; j <= temp_top + temp_height; j++) {
                                 Sum[i] += Array[j][i];
@@ -100,10 +107,11 @@ class Source {
                         int temp_sum = 0;
                         int temp_start = 0;
 
+                        // Algorytm Kadane (nieco zmodyfikowany).
+                        // Dla kazdej komorki wyliczamy maksymalna sme konczaca sie na niej.
                         for (int i = 0; i < Columns; i++) {
 
-                            // temp_sum += Sum[i];
-
+                            // Jesli poprzednia suma jest ujemna to zaczynamy sumowanie od poczatku.
                             if (Sum[i] > temp_sum + Sum[i]) {
                                 temp_start = i;
                                 temp_sum = Sum[i];
@@ -115,6 +123,8 @@ class Source {
                                 temp_start++;
                             }
 
+                            // Sprawdzenie czy aktualna (temp) pdtablica jest lepsza od tej wygrywajacej do
+                            // tej pory.
                             if (max_sum < temp_sum || (max_sum == temp_sum && !isWinning(max_top, max_top + max_height,
                                     max_left, max_right, temp_top, temp_top + temp_height, temp_start, i))) {
                                 max_sum = temp_sum;
@@ -124,20 +134,13 @@ class Source {
                                 max_height = temp_height;
                             }
 
-                            // if ((temp_sum <= 0 && (temp_sum != temp_sum - Sum[i])
-                            // || (Sum[i] == 0 && i < Columns - 1 && Sum[i + 1] != 0))) {
-                            // temp_sum = 0;
-                            // temp_start = i + 1;
-                            // }
-
                         }
 
                     }
 
                 }
+                // Obliczenie dolnego indeksu
                 int max_bottom = max_top + max_height;
-                if (max_left > max_right)
-                    max_right = max_left;
 
                 System.out.println(SetNumber + ": n=" + Lines + " m=" + Columns + ", ms= " + max_sum + ", mstab= a["
                         + max_top + ".." + max_bottom + "][" + max_left + ".." + max_right + "]");
