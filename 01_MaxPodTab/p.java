d325aed16245866c6cc1c85a106c7e3e7606a340
Magda Lipka <dev@magdalipka.com>
2021-03-16 13:44:54 +0100

    add case when first elem of subarray is zero

--- 01_MaxPodTab/p.java
+++ 01_MaxPodTab/p.java
@@ -1,138 +1,148 @@
-package MaxPodTab;
-
-//Magdalena Lipka gr.1//
+//Magdalena Lipka gr.7//
 import java.util.Scanner;
 
 class Source {
-    static Scanner scan =  new Scanner(System.in);
-    public static void main(String [] args) {
-               
-        int Quantity = scan.nextInt(); //ilosc zestawow//
 
-        while(Quantity > 0) {
-            
-            int Columns, Lines; //wymiary pojedynczej tabeli//
+    static Scanner scan = new Scanner(System.in);
+
+    static int area(int top, int bottom, int left, int right) {
+        return (bottom - top + 1) * (right - left + 1);
+    }
+
+    static boolean isWinning(int top1, int bottom1, int left1, int right1, int top2, int bottom2, int left2,
+            int right2) {
+        if (area(top1, bottom1, left1, right1) < area(top2, bottom2, left2, right2)) {
+            return true;
+        } else if (area(top1, bottom1, left1, right1) > area(top2, bottom2, left2, right2)) {
+            return false;
+        } else {
+            if (top1 < top2) {
+                return true;
+            } else if (top1 > top2) {
+                return false;
+            } else {
+                if (bottom1 < bottom2) {
+                    return true;
+                } else if (bottom1 > bottom2) {
+                    return false;
+                } else {
+                    if (left1 < left2) {
+                        return true;
+                    } else if (left1 > left2) {
+                        return false;
+                    } else {
+                        if (right1 <= right2) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int Quantity = scan.nextInt(); // ilosc zestawow//
+
+        while (Quantity > 0) {
+
+            int SetNumber; // numer zestawu
+            int Columns, Lines; // wymiary pojedynczej tabeli//
+            SetNumber = scan.nextInt();
+            scan.next();
             Lines = scan.nextInt();
             Columns = scan.nextInt();
-            
+
             int[][] Array = new int[Lines][Columns];
             int max_sum = 0;
             int max_left = 0;
-            int max_right = Columns-1;
+            int max_right = 0;
             int max_top = 0;
-            int max_height = Lines-1;
+            int max_height = 0;
 
             boolean only_neg = true;
-            boolean any_zero = false;
-            int zero_l = 0;
-            int zero_c = 0;
 
-            //wczytywanie//
-            ////
+            // wczytywanie//
 
-            for(int i = 0; i < Lines; i++) {
-                for(int j = 0; j < Columns; j++) {
-                    Array[i][j] = scan.nextInt(); 
-                    max_sum += Array[i][j];  
-                    if(Array[i][j] > 0) only_neg = false;
-                    if(Array[i][j] == 0 && !any_zero) {
-                        any_zero = true;
-                        zero_l = i;
-                        zero_c = j;   
-                    }          
+            for (int i = 0; i < Lines; i++) {
+                for (int j = 0; j < Columns; j++) {
+                    int value = scan.nextInt();
+                    Array[i][j] = value > 0 ? 3 * value : 2 * value;
+                    if (Array[i][j] >= 0)
+                        only_neg = false;
+
                 }
             }
+            max_sum = Array[0][0];
 
-            ////
-            //koniec wczytywania//
+            if (only_neg) {
+                System.out.println(SetNumber + ": n=" + Lines + " m=" + Columns + ", ms= 0, mstab is empty");
+            } else {
 
+                for (int temp_top = 0; temp_top < Lines; temp_top++) {
 
-            //wlasciwy program//
-            ////
+                    for (int temp_height = 0; temp_height + temp_top < Lines; temp_height++) {
 
-            if( only_neg && !any_zero) System.out.println("empty");
-            else{
-            if(any_zero && only_neg) {
-                max_sum = 0;
-                max_left = zero_c;
-                max_top = zero_l;
-                max_height = 0;
-                max_right = zero_c;
-                
-            } else{
-            for(int temp_top = 0; temp_top < Lines; temp_top++) {
-                
-                for(int temp_height = 0; temp_height + temp_top < Lines; temp_height++) {
+                        // sumowanie podtabeli zacznajacej sie w temp_left o szerokowi temp_width//
 
-                    //sumowanie podtabeli zacznajacej sie w temp_left o szerokowi temp_width//
-                    ////
+                        int[] Sum = new int[Columns];
+                        for (int i = 0; i < Columns; i++)
+                            Sum[i] = 0;
 
-                    int[] Sum = new int[Columns];
-                    for(int i = 0; i<Columns; i++) Sum[i]=0;
-
-                    for(int i = 0; i < Columns; i++) {
-                        for(int j = temp_top; j<=temp_top+temp_height; j++) {
-                            Sum[i] += Array[j][i];
-                        }
-                    }
-
-                    ////
-                    //koniec sumowani podtabeli//
-
-                    //liniowa maksymalna podtablica//
-                    ////
-
-                    int temp_sum = 0;
-                    int temp_start = 0;
-                
-                    
-                    
-
-                    for(int i = 0; i < Columns; i++) {
-                        
-                        temp_sum += Sum[i];
-
-                        if ( ( temp_sum <= 0 &&  (temp_sum != temp_sum - Sum[i]) || ( Sum[i] == 0 && i<Columns-1 && Sum[i+1] != 0 ) ) ) {
-                            temp_sum = 0;
-                            temp_start = i + 1;
-                        }
-
-
-                        if(temp_sum > max_sum) {
-                            max_sum = temp_sum;
-                            max_top = temp_top;
-                            max_left = temp_start;
-                            max_right = i;
-                            max_height = temp_height;
-
-
-                        }
-                        else if(temp_sum == max_sum) {
-                            if( (max_height+1)*(max_right - max_left +1) > (temp_height + 1)*(i - temp_start + 1) ) {
-                            max_sum = temp_sum;
-                            max_top = temp_top;
-                            max_left = temp_start;
-                            max_right = i;
-                            max_height = temp_height;
+                        for (int i = 0; i < Columns; i++) {
+                            for (int j = temp_top; j <= temp_top + temp_height; j++) {
+                                Sum[i] += Array[j][i];
                             }
-                            
                         }
 
-                        
+                        int temp_sum = 0;
+                        int temp_start = 0;
+
+                        for (int i = 0; i < Columns; i++) {
+
+                            // temp_sum += Sum[i];
+
+                            if (Sum[i] > temp_sum + Sum[i]) {
+                                temp_start = i;
+                                temp_sum = Sum[i];
+                            } else {
+                                temp_sum = temp_sum + Sum[i];
+                            }
+
+                            if (temp_start != i && Sum[temp_start] == 0) {
+                                temp_start++;
+                            }
+
+                            if (max_sum < temp_sum || (max_sum == temp_sum && !isWinning(max_top, max_top + max_height,
+                                    max_left, max_right, temp_top, temp_top + temp_height, temp_start, i))) {
+                                max_sum = temp_sum;
+                                max_top = temp_top;
+                                max_left = temp_start;
+                                max_right = i;
+                                max_height = temp_height;
+                            }
+
+                            // if ((temp_sum <= 0 && (temp_sum != temp_sum - Sum[i])
+                            // || (Sum[i] == 0 && i < Columns - 1 && Sum[i + 1] != 0))) {
+                            // temp_sum = 0;
+                            // temp_start = i + 1;
+                            // }
+
+                        }
+
                     }
 
-                    ////
-                    //koniec liniowej maksymalnej podtablicy//
                 }
+                int max_bottom = max_top + max_height;
+                if (max_left > max_right)
+                    max_right = max_left;
+
+                System.out.println(SetNumber + ": n=" + Lines + " m=" + Columns + ", ms= " + max_sum + ", mstab= a["
+                        + max_top + ".." + max_bottom + "][" + max_left + ".." + max_right + "]");
             }
-            }
-            int max_bottom = max_top + max_height;
-            if (max_left > max_right) max_right = max_left;
-            System.out.println("max_sum = " + max_sum + ", a[" + max_top + ".." + max_bottom + "][" + max_left + ".." + max_right + "]");
-            }
-            
-            ////
-            ////////////////
+
             Quantity--;
         }
     }
--- 07_DNAsort/Source.java
+++ 07_DNAsort/Source.java
@@ -1,3 +1,4 @@
+
 //Lipka Magdalena gr.1
 import java.util.Scanner;
 
@@ -9,7 +10,7 @@ class Codon {
 
 	Codon(String inn) {
 		Data = inn;
-		quantity = Data.length()/3;
+		quantity = Data.length() / 3;
 		Codons = new String[quantity];
 		quantity = 0;
 	}
@@ -19,120 +20,131 @@ class Codon {
 	}
 
 	boolean isStop(int i) {
-		if ( Data.toCharArray()[i] == 'T' && ( (Data.toCharArray()[i+1] == 'A' && Data.toCharArray()[i+2] == 'A') || (Data.toCharArray()[i+1] == 'A' && Data.toCharArray()[i+2] == 'G') || (Data.toCharArray()[i+1] == 'G' && Data.toCharArray()[i+2] == 'A') ) ) return true;
+		if (Data.toCharArray()[i] == 'T' && ((Data.toCharArray()[i + 1] == 'A' && Data.toCharArray()[i + 2] == 'A')
+				|| (Data.toCharArray()[i + 1] == 'A' && Data.toCharArray()[i + 2] == 'G')
+				|| (Data.toCharArray()[i + 1] == 'G' && Data.toCharArray()[i + 2] == 'A')))
+			return true;
 		return false;
 	}
 
 	boolean isStart(int i) {
-		if (Data.toCharArray()[i] == 'A' && Data.toCharArray()[i+1] == 'T' && Data.toCharArray()[i+2] == 'G') return true;
-		return false; 
+		if (Data.toCharArray()[i] == 'A' && Data.toCharArray()[i + 1] == 'T' && Data.toCharArray()[i + 2] == 'G')
+			return true;
+		return false;
 	}
 
-	boolean isBigger (String a, String b) {
-		if ( a.toCharArray()[0] > b.toCharArray()[0] ) return true;
-		else if ( a.toCharArray()[0] == b.toCharArray()[0] ) {
-			if ( a.toCharArray()[1] > b.toCharArray()[1] ) return true;
-			else if ( a.toCharArray()[1] == b.toCharArray()[1] ) {
-				if ( a.toCharArray()[2] > b.toCharArray()[2] ) return true;
-				else if ( a.toCharArray()[2] == b.toCharArray()[2] ) {
-					if ( a.toCharArray()[3] > b.toCharArray()[3] ) return true;
-					else return false;
-				}
-				else return false;
-			}
-			else return false;
-		}  
-		else return false;
+	boolean isBigger(String a, String b) {
+		if (a.toCharArray()[0] > b.toCharArray()[0])
+			return true;
+		else if (a.toCharArray()[0] == b.toCharArray()[0]) {
+			if (a.toCharArray()[1] > b.toCharArray()[1])
+				return true;
+			else if (a.toCharArray()[1] == b.toCharArray()[1]) {
+				if (a.toCharArray()[2] > b.toCharArray()[2])
+					return true;
+				else if (a.toCharArray()[2] == b.toCharArray()[2]) {
+					if (a.toCharArray()[3] > b.toCharArray()[3])
+						return true;
+					else
+						return false;
+				} else
+					return false;
+			} else
+				return false;
+		} else
+			return false;
 	}
 
-	boolean isSomewhatBigger ( String a, char b, int pos ) {
-		if (a.toCharArray()[pos] > b) return true;
-		else return false;
+	boolean isSomewhatBigger(String a, char b, int pos) {
+		if (a.toCharArray()[pos] > b)
+			return true;
+		else
+			return false;
 	}
 
-	boolean Validate () {
+	boolean Validate() {
 
 		toUpper();
 
-		//sprawdzenie czy nie ma nieprawidlowych znaczkow
-		for ( int i = 0; i < Data.length(); i++ ) {
-			if ( Data.toCharArray()[i] != 'A' &&  Data.toCharArray()[i] != 'C' &&  Data.toCharArray()[i] != 'T' &&  Data.toCharArray()[i] != 'G' ) {
+		// sprawdzenie czy nie ma nieprawidlowych znaczkow
+		for (int i = 0; i < Data.length(); i++) {
+			if (Data.toCharArray()[i] != 'A' && Data.toCharArray()[i] != 'C' && Data.toCharArray()[i] != 'T'
+					&& Data.toCharArray()[i] != 'G') {
 				System.out.println("Wrong character in DNA sequence.");
 				return false;
 			}
 		}
 
-		//pierwsza pozycja startu
+		// pierwsza pozycja startu
 		int start = -1;
-		for ( int i = 0; i < Data.length()-2; i++ ) {
-			if ( isStart(i) ) {
+		for (int i = 0; i < Data.length() - 2; i++) {
+			if (isStart(i)) {
 				start = i;
 				break;
 			}
 		}
-		if ( start == -1 ) {
+		if (start == -1) {
 			System.out.println("Wrong DNA sequence.");
 			return false;
 		}
 
-		//ostatnia pozycja konca
+		// ostatnia pozycja konca
 		int koniec = -1;
-		for ( int i = start + 3; i < Data.length()-2; i += 3 ) {
-			if ( isStop(i) ) {
+		for (int i = start + 3; i < Data.length() - 2; i += 3) {
+			if (isStop(i)) {
 				koniec = i;
 			}
 		}
-		if ( koniec == -1 ) {
+		if (koniec == -1) {
 			System.out.println("Wrong DNA sequence.");
 			return false;
 		}
-		if ( koniec == start + 3 ) {
+		if (koniec == start + 3) {
 			System.out.println("Wrong DNA sequence.");
 			return false;
 		}
 
-		//sprawdzenie powtorzen startu
-		for ( int i = start+3; i < koniec; i += 3 ) {
-			if ( isStart(i) ) {
+		// sprawdzenie powtorzen startu
+		for (int i = start + 3; i < koniec; i += 3) {
+			if (isStart(i)) {
 				System.out.println("More than one START/STOP codon.");
 				return false;
 			}
 		}
 
-		//sprawdzenie powtorzen konca 
-		for ( int i = koniec-3; i > start; i -= 3 ) {
-			if ( isStop(i) ) {
+		// sprawdzenie powtorzen konca
+		for (int i = koniec - 3; i > start; i -= 3) {
+			if (isStop(i)) {
 				System.out.println("More than one START/STOP codon.");
 				return false;
 			}
 		}
 
-
-		//przepiasnie poprawnych danych do tabelki
-		for ( int i = start + 3; i < koniec; i += 3 ) {
+		// przepiasnie poprawnych danych do tabelki
+		for (int i = start + 3; i < koniec; i += 3) {
 			String Pom = Character.toString(Data.toCharArray()[i]);
-			Pom += Character.toString(Data.toCharArray()[i+1]);
-			Pom += Character.toString(Data.toCharArray()[i+2]);
+			Pom += Character.toString(Data.toCharArray()[i + 1]);
+			Pom += Character.toString(Data.toCharArray()[i + 2]);
 			Codons[quantity] = Pom;
 			quantity++;
 		}
-		
+
 		return true;
-	
+
 	}
 
-	void swap (int m, int n) {
+	void swap(int m, int n) {
 		String pom = Codons[m];
 		Codons[m] = Codons[n];
 		Codons[n] = pom;
 	}
 
-	int partition (char klucz, int left, int right, int depth ) {
-	
+	int partition(char klucz, int left, int right, int depth) {
+
 		int i = left - 1;
 
-		for ( int j = left; j <= right; j++ ) {
-			if ( !isSomewhatBigger(Codons[j], klucz, depth) ) {
+		for (int j = left; j <= right; j++) {
+			if (!isSomewhatBigger(Codons[j], klucz, depth)) {
 				i++;
 				swap(i, j);
 			}
@@ -141,66 +153,76 @@ class Codon {
 	}
 
 	char genKey(int i) {
-		if ( i%4 == 0 ) return 'A';
-		else if ( i%4 == 1 ) return 'C';
-		else if ( i%4 == 2 ) return 'G';
-		else return 'T'; 
+		if (i % 4 == 0)
+			return 'A';
+		else if (i % 4 == 1)
+			return 'C';
+		else if (i % 4 == 2)
+			return 'G';
+		else
+			return 'T';
 	}
 
-	int findBorder ( int start, int depth ) {
-		for ( int i = start; i < quantity; i++ ) {
-			if ( i < quantity-1 && Codons[i].toCharArray()[depth] != Codons[i+1].toCharArray()[depth]  || depth > 0 && i < quantity-1 && Codons[i].toCharArray()[depth-1] != Codons[i+1].toCharArray()[depth-1] ) return i;
+	int findBorder(int start, int depth) {
+		for (int i = start; i < quantity; i++) {
+			if (i < quantity - 1 && Codons[i].toCharArray()[depth] != Codons[i + 1].toCharArray()[depth] || depth > 0
+					&& i < quantity - 1 && Codons[i].toCharArray()[depth - 1] != Codons[i + 1].toCharArray()[depth - 1])
+				return i;
 		}
 		return quantity - 1;
 	}
 
-	void sort () {
- 
+	void sort() {
+
 		int left = 0;
-		int right = quantity-1;
+		int right = quantity - 1;
 		int Border = -1;
 		char key = 'T';
 		int licznik = 0;
 
-		//przesortowanie ze wzgledu na pierwszy znak
-		while ( left < right ) {
+		// przesortowanie ze wzgledu na pierwszy znak
+		while (left < right) {
 			key = genKey(licznik);
 			int temp = partition(key, left, right, 0);
-			if ( temp > left ) left = temp;
+			if (temp > left)
+				left = temp;
 			licznik++;
 		}
-		//sortowanie ze wzgledu na pozostale znaki
-		for ( int depth = 1; depth < 3; depth++ ) {
+		// sortowanie ze wzgledu na pozostale znaki
+		for (int depth = 1; depth < 3; depth++) {
 			left = 0;
 			licznik = 3;
 			key = genKey(licznik);
-			while( left < right ) {
-				if ( key == 'T' ) Border = findBorder(left, depth-1);
+			while (left < right) {
+				if (key == 'T')
+					Border = findBorder(left, depth - 1);
 				licznik++;
 				key = genKey(licznik);
 				int temp = partition(key, left, Border, depth);
-				if ( temp > left ) left = temp;
-				if ( key == 'T' ) left = Border+1;
-			}			
+				if (temp > left)
+					left = temp;
+				if (key == 'T')
+					left = Border + 1;
+			}
 		}
 	}
 
-	void move (int start, int koniec) {
+	void move(int start, int koniec) {
 		String Temp = Codons[koniec];
 		int i = koniec;
-		while ( i > start ) {
-			Codons[i] = Codons[i-1];
+		while (i > start) {
+			Codons[i] = Codons[i - 1];
 			i--;
 		}
 		Codons[i] = Temp;
 	}
 
-	void stablepartition (char klucz, int depth ) {
+	void stablepartition(char klucz, int depth) {
 
 		int i = 0;
 
-		for ( int j = 0; j < quantity; j++ ) {
-			if ( !isSomewhatBigger(Codons[j], klucz, depth) ) {
+		for (int j = 0; j < quantity; j++) {
+			if (!isSomewhatBigger(Codons[j], klucz, depth)) {
 				move(i, j);
 				i++;
 			}
@@ -209,16 +231,16 @@ class Codon {
 
 	void stablesort() {
 
-		for ( int i = 0; i < 3; i++ ) {
-			stablepartition('A', 2-i);
-			stablepartition('C', 2-i);
-			stablepartition('G', 2-i);
+		for (int i = 0; i < 3; i++) {
+			stablepartition('A', 2 - i);
+			stablepartition('C', 2 - i);
+			stablepartition('G', 2 - i);
 		}
 
 	}
 
 	void print() {
-		for ( int i = 0; i < quantity; i++ ) {
+		for (int i = 0; i < quantity; i++) {
 			System.out.print(Codons[i]);
 		}
 		System.out.print("\n");
@@ -228,17 +250,18 @@ class Codon {
 
 class Source {
 	public static Scanner scan = new Scanner(System.in);
-	public static void main ( String[] args  ) {
+
+	public static void main(String[] args) {
 
 		int Prompts = scan.nextInt();
 
-		for ( int i = 0; i < Prompts; i++ ) {
+		for (int i = 0; i < Prompts; i++) {
 
 			String Input = scan.next();
 
 			Codon KODON = new Codon(Input);
 
-			if ( KODON.Validate() ) {
+			if (KODON.Validate()) {
 
 				KODON.stablesort();
 				KODON.print();
